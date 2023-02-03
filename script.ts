7776f5827d9194712edc843ca75507614eb41913
Magdalena Sedkiewicz <magdalenainspirations@gmail.com>
2023-02-03 21:08:02 +0100

    New exercise

--- script.ts
+++ script.ts
@@ -1,10 +1,21 @@
 {
-    const btnElem = document.querySelector('.sum-btn') as HTMLButtonElement;
-    const numInput = document.querySelector('.num-input') as HTMLInputElement;
+    const persons = [];
 
-    function sum(a: number, b: number) {
-        console.log(a + b)
-      }
+    function addPerson(firstName, lastName) {
+        const name = firstName + lastName;
+        persons.push(name);
+    }
 
-      btnElem.addEventListener('click', () => { sum(5, +numInput.value) })
+    let firstName = 'John';
+    let lastName = 'Doe';
+    let age = 18;
+
+    const person = {
+        firstName,
+        lastName
+    }
+
+    addPerson(person.firstName, person.lastName);
+    addPerson('Amanda', 'Doe');
+    addPerson('Thomas', 'Jefferson');
 }
