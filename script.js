f1a203a8caf1567bd0843db0639a7aed69419820
Magdalena Sedkiewicz <magdalenainspirations@gmail.com>
2023-02-03 20:55:49 +0100

    Declare number type

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
-var btnElem = document.querySelector('.sum-btn');
-var numInput = document.querySelector('.num-input');
-function sum(a, b) {
-    console.log(a + b);
+{
+    var btnElem = document.querySelector('.sum-btn');
+    var numInput_1 = document.querySelector('.num-input');
+    function sum(a, b) {
+        console.log(a + b);
+    }
+    btnElem.addEventListener('click', function () { sum(5, numInput_1.value); });
 }
-btnElem.addEventListener('click', function () { sum(5, numInput.value); });
--- script.ts
+++ script.ts
@@ -2,9 +2,9 @@
     const btnElem = document.querySelector('.sum-btn') as HTMLButtonElement;
     const numInput = document.querySelector('.num-input') as HTMLInputElement;
 
-    function sum(a, b) {
-    console.log(a + b);
-    }
+    function sum(a: number, b: number) {
+        console.log(a + b)
+      }
 
     btnElem.addEventListener('click', () => { sum(5, numInput.value) });
 }
