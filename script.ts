89c707ce060cabf77706f3c9ad2cb76e38522282
Magdalena Sedkiewicz <magdalenainspirations@gmail.com>
2023-02-03 21:26:49 +0100

    New exercise

--- script.ts
+++ script.ts
@@ -1,52 +1,7 @@
-{
-    enum Roles {
-        Admin,
-        Moderator,
-        User,
-    }
-
-    const JohnDoe: {
-        id: string | number;
-        firstName: string;
-        lastName: string;
-        role: Roles;
-    }
-        = {
-        id: 1,
-        firstName: 'John',
-        lastName: 'Doe',
-        role: Roles.Admin
-    }
-
-
-    const AmandaDoe: {
-        id: string | number;
-        firstName: string;
-        lastName: string;
-        role: Roles;
-    }
-        = {
-        id: 2,
-        firstName: 'John',
-        lastName: 'Doe',
-        role: Roles.Moderator
-    }
-
-
-    const ThomasJefferson: {
-        id: string | number;
-        firstName: string;
-        lastName: string;
-        role: Roles;
-    }
-        = {
-        id: 'rwe5345sfst3453543',
-        firstName: 'John',
-        lastName: 'Doe',
-        role: Roles.User
-    }
-
-
-    const names: [string, string, string] = [JohnDoe.firstName, AmandaDoe.firstName, ThomasJefferson.firstName]
-    const JohnDoeSummary: [string, string, Roles] = [JohnDoe.firstName, JohnDoe.lastName, JohnDoe.role]
+function join(valOne, valTwo) {
+    return valOne + valTwo;
 }
+
+const one = join('John', 'Doe'); // should return JohnDoe
+const two = join(5, 6); // should return 11
+const three = join(5, '5'); // should return false
