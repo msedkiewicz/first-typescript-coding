3c4458a3bab2e01fadf3e0c7e9065f10a48336dc
Magdalena Sedkiewicz <magdalenainspirations@gmail.com>
2023-02-05 13:58:38 +0100

    Fix function working

--- script.js
+++ script.js
@@ -1,28 +1,13 @@
 {
-    var Roles = void 0;
-    (function (Roles) {
-        Roles[Roles["Admin"] = 0] = "Admin";
-        Roles[Roles["Moderator"] = 1] = "Moderator";
-        Roles[Roles["User"] = 2] = "User";
-    })(Roles || (Roles = {}));
-    var JohnDoe = {
-        id: 1,
-        firstName: 'John',
-        lastName: 'Doe',
-        role: Roles.Admin
-    };
-    var AmandaDoe = {
-        id: 2,
-        firstName: 'John',
-        lastName: 'Doe',
-        role: Roles.Moderator
-    };
-    var ThomasJefferson = {
-        id: 'rwe5345sfst3453543',
-        firstName: 'John',
-        lastName: 'Doe',
-        role: Roles.User
-    };
-    var names = [JohnDoe.firstName, AmandaDoe.firstName, ThomasJefferson.firstName];
-    var JohnDoeSummary = [JohnDoe.firstName, JohnDoe.lastName, JohnDoe.role];
+    function join(valOne, valTwo) {
+        if (typeof valOne === 'number' && typeof valTwo === 'number') {
+            return valOne + valTwo;
+        }
+        else if (typeof valOne === 'string' && typeof valTwo === 'string') {
+            return valOne.toString() + valTwo.toString();
+        }
+    }
+    var one = join('John', 'Doe'); // should return JohnDoe
+    var two = join(5, 6); // should return 11
+    var three = join(5, '5'); // should return false
 }
--- script.ts
+++ script.ts
@@ -1,7 +1,10 @@
-function join(valOne, valTwo) {
-    return valOne + valTwo;
-}
+{
+    function join(valOne: number | string, valTwo: number | string) {
+        if (typeof valOne === 'number' && typeof valTwo === 'number') {return valOne + valTwo;}
+        else if (typeof valOne === 'string' && typeof valTwo === 'string') {return valOne.toString() + valTwo.toString();}
+    }
 
-const one = join('John', 'Doe'); // should return JohnDoe
-const two = join(5, 6); // should return 11
-const three = join(5, '5'); // should return false
+    const one = join('John', 'Doe') // should return JohnDoe
+    const two = join(5, 6) // should return 11
+    const three = join(5, '5') // should return false
+}
