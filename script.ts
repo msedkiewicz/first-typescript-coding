25b986411bc31a7b0788ecc023ce050d41a325b3
Magdalena Sedkiewicz <magdalenainspirations@gmail.com>
2023-02-05 13:59:30 +0100

    Code formatting

--- script.ts
+++ script.ts
@@ -1,7 +1,7 @@
 {
     function join(valOne: number | string, valTwo: number | string) {
-        if (typeof valOne === 'number' && typeof valTwo === 'number') {return valOne + valTwo;}
-        else if (typeof valOne === 'string' && typeof valTwo === 'string') {return valOne.toString() + valTwo.toString();}
+        if (typeof valOne === 'number' && typeof valTwo === 'number') { return valOne + valTwo; }
+        else if (typeof valOne === 'string' && typeof valTwo === 'string') { return valOne.toString() + valTwo.toString(); }
     }
 
     const one = join('John', 'Doe') // should return JohnDoe
